97e6bdca9f936804d1f464cf175ca33c68b46ac4
Rohit Singh <singhrohit2@hotmail.com>
2017-12-24 17:10:56 -0700

    formatting

--- src/org/team5763/ConsoleInterceptor.java
+++ src/org/team5763/ConsoleInterceptor.java
@@ -2,17 +2,17 @@ package org.team5763;
 
 import java.io.PrintStream;
 
-public class ConsoleInterceptor extends PrintStream{
-	boolean error;
-	public ConsoleInterceptor(boolean error) {
-		super(System.out);
-		this.error=error;
-	}
-	public void print(String s){
-		if(!error){
-			Bolt.writeConsole(s);
-		}else{
-			Bolt.writeError(s);
-		}
-	}
+public class ConsoleInterceptor extends PrintStream {
+    boolean error;
+    public ConsoleInterceptor(boolean error) {
+        super(System.out);
+        this.error = error;
+    }
+    public void print(String s) {
+        if (!error) {
+            Bolt.writeConsole(s);
+        } else {
+            Bolt.writeError(s);
+        }
+    }
 }
--- src/org/team5763/NetworkTester.java
+++ src/org/team5763/NetworkTester.java
@@ -5,45 +5,46 @@ import java.util.Scanner;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class NetworkTester {
-	public NetworkTester() throws InterruptedException{
-		NetworkTable table=Bolt.listener.table;
-		Scanner scan=new Scanner(System.in);
-		while(true){
-			System.out.print(">");
-			String seq=scan.next();
-			if(seq.equalsIgnoreCase("exit")){
-				scan.close();
-				System.exit(0);
-			}else if(seq.equalsIgnoreCase("rand")){
-				for(int i=0;i<100;i++){
-					table.putNumber("fY", 1-2*Math.random());
-					table.putNumber("fX", 1-2*Math.random());
-					Thread.sleep(50);
-				}
-			}else{
-				System.out.print("Running custom command: ");
-				table.putNumber("fX",0);
-				table.putNumber("fY",0);
-				for(int i=0;i<seq.length();i++){
-					String cmd=seq.substring(i, i+1);
-					System.out.print(cmd);
-					if(cmd.equals("w")){
-						table.putNumber("fY", 1);
-					}else if(cmd.equals("a")){
-						table.putNumber("fX", -1);
-					}else if(cmd.equals("s")){
-						table.putNumber("fY", -1);
-					}else if(cmd.equals("d")){
-						table.putNumber("fX", 1);
-					}
-				}
-				System.out.print("\n");
-			}
-		}
-	}
-	public static void main(String[] args) throws InterruptedException {
-		Bolt.main(null);
-		System.out.println("Debug/Test mode\nCommands:\n\tRAND - Random joystick motion for 5s\n\tWASD - Move the joystick in one direction\n\tEXIT - Safely close");
-		new NetworkTester();
-	}
+    public NetworkTester() throws InterruptedException {
+        NetworkTable table = Bolt.listener.table;
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.print(">");
+            String seq = scan.next();
+            if (seq.equalsIgnoreCase("exit")) {
+                scan.close();
+                System.exit(0);
+            } else if (seq.equalsIgnoreCase("rand")) {
+                for (int i = 0; i < 100; i++) {
+                    table.putNumber("fY", 1 - 2 * Math.random());
+                    table.putNumber("fX", 1 - 2 * Math.random());
+                    Thread.sleep(50);
+                }
+            } else {
+                System.out.print("Running custom command: ");
+                table.putNumber("fX", 0);
+                table.putNumber("fY", 0);
+                for (int i = 0; i < seq.length(); i++) {
+                    String cmd = seq.substring(i, i + 1);
+                    System.out.print(cmd);
+                    if (cmd.equals("w")) {
+                        table.putNumber("fY", 1);
+                    } else if (cmd.equals("a")) {
+                        table.putNumber("fX", -1);
+                    } else if (cmd.equals("s")) {
+                        table.putNumber("fY", -1);
+                    } else if (cmd.equals("d")) {
+                        table.putNumber("fX", 1);
+                    }
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Bolt.main(null);
+        System.out.println(
+            "Debug/Test mode\nCommands:\n\tRAND - Random joystick motion for 5s\n\tWASD - Move the joystick in one direction\n\tEXIT - Safely close");
+        new NetworkTester();
+    }
 }
